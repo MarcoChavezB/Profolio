fbf5cf4f026e3282e34bc98dff6d29f61aacbe55
MarcoChavezB <marco1102004@gmail.com>
2024-05-01 22:09:39 -0600

    añadiendo resposividad

---
+++ src/components/controles/links.vue
@@ -0,0 +1,15 @@
+<template>
+    <article class="cont-color">   
+        <slot></slot>
+    </article>
+</template>
+
+<style scoped>
+article {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 4px;
+    border-radius: 10px;
+}
+</style>
